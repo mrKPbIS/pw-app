8b0145c538bdf1060f59bc58f852b6da128fd380
Andrey Strygin <mr.kpbis@gmail.com>
2023-08-14 18:03:36 +0300

    add cors settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, {} from 'express';
+import cors from 'cors';
 import transactionRouter from './transactions/transaction.router';
 import { errorHandler } from './middleware/errors.middleware';
 import authRouter from './users/auth.router';
@@ -6,10 +7,13 @@ import userRouter from './users/user.router';
 
 const app = express();
 
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
-app.use('/auth', authRouter);
-app.use('/users', userRouter);
-app.use('/transactions', transactionRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/transactions', transactionRouter);
 app.use('/ping', (req, res) => {
   res.send('pong');
 });
