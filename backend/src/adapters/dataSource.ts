16898bfa3bd0608dbd1f90b1005630873c1e5725
Andrey Strygin <mr.kpbis@gmail.com>
2023-09-08 12:57:14 +0300

    use datasource isInitialized

--- backend/src/adapters/dataSource.ts
+++ backend/src/adapters/dataSource.ts
@@ -2,14 +2,12 @@ import { DataSource, Repository, DataSourceOptions } from 'typeorm';
 
 export class AppDataSource {
   private dataSource: DataSource;
-  private isDataSourceInitialized: boolean;
   private entities: string[];
   private repositories: Map<string, Repository<any>>;
 
   constructor(dataSourceConfig: DataSourceOptions, entities: string[]) {
     this.entities = entities;
     this.dataSource = new DataSource(dataSourceConfig);
-    this.isDataSourceInitialized = false;
     this.repositories = new Map();
   }
 
@@ -17,7 +15,6 @@ export class AppDataSource {
     try {
       await this.dataSource.initialize();
       console.log('Data source initialized');
-      this.isDataSourceInitialized = true;
       for(const entity of this.entities) {
         this.repositories.set(entity, await this.dataSource.getRepository(entity));
       }
@@ -28,12 +25,8 @@ export class AppDataSource {
     }
   }
 
-  isInitialized() {
-    return this.isDataSourceInitialized;
-  }
-
   getRepository(entity: string): Repository<any> | null {
-    if (!this.isDataSourceInitialized) {
+    if (!this.dataSource.isInitialized) {
       return null;
     }
     return this.repositories.get(entity);
