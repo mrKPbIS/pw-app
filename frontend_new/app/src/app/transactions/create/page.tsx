203c0b83fadd17a038e09225cc4cfc707ad14070
Andrey Strygin <mr.kpbis@gmail.com>
2023-12-12 18:41:28 +0300

    Add create transaction route

--- frontend_new/app/src/app/transactions/create/page.tsx
+++ frontend_new/app/src/app/transactions/create/page.tsx
@@ -1,6 +1,47 @@
-import { Box, Container, AppBar, Button, Typography, Paper, Toolbar } from '@mui/material';
+'use client';
 
-export default function TransactionCreateForm() {
+import { getUsers, createTransaction } from '@/app/api/api';
+import { getToken, isAuthenticated } from '@/app/api/auth';
+import { Box, Container, AppBar, Button, Typography, Paper, Toolbar, ListItem, TextField, List, ListItemText, Autocomplete } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { SyntheticEvent, useEffect, useState } from 'react';
+
+export default function TransactionCreateForm(props: any) {
+  console.log(props);
+  const { clone } = props;
+
+  const [amount, setAmount] = useState(clone? clone.amount: '');
+  const [recipient, setRecipient] = useState(clone? clone.recipientId: '');
+  const [usersList, setUsersList] = useState(new Array());
+  const router = useRouter();
+
+  const token = getToken();
+  
+  if (!isAuthenticated()) {
+    router.push('/login');
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await getUsers(token);
+      if (req.success && req.data) {
+        const list = req.data.users.map(({ id, name}) => { return { label: name, id }});
+        console.log(list === usersList, usersList, list)
+        setUsersList(list);
+      }
+    }
+
+    fetchData();
+  }, [JSON.stringify(usersList)]);
+
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    const req = await createTransaction({ amount, recipientId: recipient }, token);
+    if (req.success && req.data) {
+      router.push('/profile');
+    }
+  }
+
   return (
     <Box>
       <AppBar position='static'>
@@ -13,8 +54,25 @@ export default function TransactionCreateForm() {
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Button variant="contained" href='/profile'>Back</Button>
-          
+          <form onSubmit={handleSubmit}>
+            <List>
+              <Button variant="contained" href='/profile'>Back</Button>
+              <ListItem>
+                <ListItemText primary="Recipient"/>
+                <Autocomplete
+                  id='transactions-recipient'
+                  options={usersList}
+                  onChange={(e, value) => { setRecipient(value.id)}}
+                  renderInput={(params) => <TextField {...params} label="Recipient"/>}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Amount"/>
+                <TextField id='transaction-amount' label='amount' variant="filled" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+              </ListItem>
+              <Button variant='contained' type='submit'>Create Transaction</Button>
+            </List>
+          </form>
         </Paper>
       </Container>
     </Box>
